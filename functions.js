4631eae4cf4c90cf573e0140f042f4ed805f64f4
Cerebellum8 <garnshev@gmail.com>
2025-09-05 15:00:30 -0500

    func

--- functions.js
+++ functions.js
@@ -9,30 +9,63 @@
  * and read every instruction carefully.
  */
 
+
+
 //////////////////////////////////////////////////////////////////////
 // Step 1 - Search ///////////////////////////////////////////////////
-
- // hi why are you trippging  
-    
-
+function search(animals, name) {
+    for (var i = 0; i < animals.length; i++){
+        if (animals[i].name === name){
+            return animals[i];
+        }
+    }
+     return null;
+  }
+ 
 
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Replace //////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-
+function replace(animals, name, replacement){
+  for(var i = 0; i < animals.length; i++){
+    if (animals[i].name === name){
+        animals[i] = replacement;
+        return;
+    }
+  }  
+}
 
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Remove ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+function remove(animals, name){
+for (var i = 0; i < animals.length; i++){
+    if(animals[i].name === name){
+       animals.splice(i, 1);
+        return;
+    }
+}
+}
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 4 - Add ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
-
+function add(animals, animal){
+    if (animal.name.length === 0){
+        return;
+    }
+    if (animal.species.length === 0){
+        return;
+    }
+      for (var i = 0; i < animals.length; i++){
+        if (animals[i].name === animal.name){
+          return;
+        }
+      }
+      animals.push(animal);
+    }
 
 /**
  * You did it! You're all done with Matchy!
